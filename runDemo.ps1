91c888cbaf6f04ca271169caf50d10ccd1cbf8e0
npickard <npickard@platformq.com>
2017-02-23 15:31:21 -0500

    added docker stuff

--- runDemo.ps1
+++ runDemo.ps1
@@ -26,7 +26,6 @@ $mappedHostInfo = $mappedHostName + ':' + $ip.ipaddress[0]
 
 #set docker sql name
 $mySQLName = $demoName + '-' + 'MySQL'
-
 Write-Host 'The mysql container name is '$mySQLName
 
 #first stop and remove any existing SQL containers
@@ -35,7 +34,21 @@ docker stop $mySQLName
 docker rm $mySQLName
 
 # note: -p host port to the container port
-docker run --name=$mySQLName -p $portSetting -e MYSQL_ROOT_PASSWORD=password -d mysql:5.6.32
+$cmd = 'docker run --name=' + $mySQLName + ' -p ' + $portSetting + ' -e MYSQL_ROOT_PASSWORD=password -d mysql:5.6.32'
+Write-Host $cmd
+Invoke-Expression $cmd
+
+#make sure container is started
+$cmd = 'Start-Sleep -s 5'
+Write-Host $cmd
+Invoke-Expression $cmd
+
+
+# create schema
+$cmd = 'cmd /c ''docker exec -i ' + $mySQLName + ' mysql --port 3306 --password=password < createSchema.sql'''
+Write-Host $cmd
+Invoke-Expression $cmd
+
 
 
 #--------------------------------------------------------------------------------------
@@ -49,5 +62,14 @@ docker stop $myDemoName
 docker rm $myDemoName
 
 #we now have the mysql running, now run the demo app
-#docker run --name=mainApp --add-host local.omedlive.com:$ip.ipaddress[0] -p 8080:8080 --env-file=$Env:ZAREX_DOCKER_PATH\docker\local.env -d -v $Env:ZAREX_DOCKER_PATH\main-app\target\main-app-1.0-SNAPSHOT.jar:/docker/main-app-1.0-SNAPSHOT.jar java:8u40 java -jar /docker/main-app-1.0-SNAPSHOT.jar
-docker run --name=$myDemoName --add-host $mappedHostInfo -p 8080:8080 -d -v $Env:DEMO_HOME\target\demo-0.0.1-SNAPSHOT.jar:/docker/demo-0.0.1-SNAPSHOT.jar java:8u40 java -jar /docker/demo-0.0.1-SNAPSHOT.jar
+$cmd = 'docker run --name=' + $myDemoName + ' --add-host ' + $mappedHostInfo + ' -p 8080:8080 -d -v $Env:DEMO_HOME\target\demo-0.0.1-SNAPSHOT.jar:/docker/demo-0.0.1-SNAPSHOT.jar java:8u40 java -jar /docker/demo-0.0.1-SNAPSHOT.jar'
+Write-Host $cmd
+Invoke-Expression $cmd
+
+
+#--------------------------------------------------------------------------------------
+# make sure APP uses MYSQL
+#
+$cmd = 'sudo docker run --name ' + $myDemoName + ' --link ' + $mySQLName + ':' + $mySQLName + ' -p 8080:8080 -d tomcat:7-jre8'
+Write-Host $cmd
+Invoke-Expression $cmd
--- test.ps1
+++ test.ps1
@@ -7,4 +7,28 @@
 #
 #############################################
 
-echo hello
+# usage will be:  runMyDemo [demoName] [port]
+
+# Obtain the ip address
+$ip=get-WmiObject Win32_NetworkAdapterConfiguration|Where {$_.Ipaddress.length -gt 1}
+
+$demoName = 'myDemo'
+$hostPort = '3308'
+$containerPort = '3306'
+$portSetting = $hostPort + ':' + $containerPort
+$mappedHostName = 'local.' + $demoName + '.com'
+$mappedHostInfo = $mappedHostName + ':' + $ip.ipaddress[0]
+
+
+#--------------------------------------------------------------------------------------
+# MYSQL
+#
+
+#set docker sql name
+$mySQLName = $demoName + '-' + 'MySQL'
+Write-Host 'The mysql container name is '$mySQLName
+
+$cmd = 'cmd /c ''docker exec -i ' + $mySQLName + ' mysql --port 3306 --password=password < createSchema.sql'''
+Write-Host $cmd
+Invoke-Expression $cmd
+
